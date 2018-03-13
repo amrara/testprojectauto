40db4cf88dfe7a082207b147e45f5941135f5cc5
Amra Hadzibajric <amra.hadzibajric@AuthorityPartners.com>
2018-03-13 14:35:01 +0100

    Test

---
+++ ClassLibrary1/ClassLibrary1/Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Test
+    {
+        [Test]
+        public void TestappOne()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.klix.ba";
+            driver.FindElement(By.XPath("//*[@id='kat_naslovnica']/header/div[1]/nav/ul[2]/li[2]/div/a"));
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestappTwo()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.klix.ba";
+            driver.FindElement(By.XPath("//*[@id='kat_naslovnica']/header/div[1]/nav/ul[1]/li[5]/div/a"));
+            driver.Quit();
+        }
+
+    }
+}
