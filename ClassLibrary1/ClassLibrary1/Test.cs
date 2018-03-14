d778f108f1855bd1ce70e3d7e7e679e76da9a57c
Jasmin Dacic <jasmin.dacic@authoritypartners.com>
2018-03-14 17:02:41 +0100

    Added selenium grid configuration

--- ClassLibrary1/ClassLibrary1/Test.cs
+++ ClassLibrary1/ClassLibrary1/Test.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,25 +10,43 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Test
+    [TestFixture]
+    [Parallelizable]
+    public class Test : TestsSetupClass
     {
         [Test]
+        [Parallelizable]
+
         public void TestappOne()
         {
-            IWebDriver driver = new ChromeDriver();
             driver.Url = "http://www.klix.ba";
             driver.FindElement(By.XPath("//*[@id='kat_naslovnica']/header/div[1]/nav/ul[2]/li[2]/div/a"));
-            driver.Quit();
         }
 
         [Test]
+        [Parallelizable]
+
         public void TestappTwo()
         {
-            IWebDriver driver = new ChromeDriver();
             driver.Url = "http://www.klix.ba";
             driver.FindElement(By.XPath("//*[@id='kat_naslovnica']/header/div[1]/nav/ul[1]/li[5]/div/a"));
-            driver.Quit();
         }
 
+        [Test]
+        [Parallelizable]
+        public void TestappThree()
+        {
+            driver.Url = "http://www.klix.ba";
+            driver.FindElement(By.XPath("//*[@id='kat_naslovnica']/header/div[1]/nav/ul[1]/li[5]/div/a"));
+        }
+
+        [Test]
+        [Parallelizable]
+
+        public void TestappFour()
+        {
+            driver.Url = "http://www.klix.ba";
+            driver.FindElement(By.XPath("//*[@id='kat_naslovnica']/header/div[1]/nav/ul[1]/li[5]/div/a"));
+        }
     }
 }
---
+++ ClassLibrary1/ClassLibrary1/TestsSetupClass.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace ClassLibrary1
+{
+    [SetUpFixture]
+    public class TestsSetupClass
+    {
+        protected IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            var capabilities = new DesiredCapabilities("chrome", string.Empty, new Platform(PlatformType.Any));
+            driver = new RemoteWebDriver(new Uri("http://10.2.115.139:4445/wd/hub"), capabilities);
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTeardown()
+        {
+            driver.Quit();
+        }
+    }
+}
